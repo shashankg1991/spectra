bbeb6f9a95362dbd245fca26ed755140f8d97d0f
Shashank Gupta <shashank.gupta@smith.co>
2020-05-22 18:11:54 -0400

    Changed DB to Azure

--- src/main/java/com/spectra/jewel/JewelApplication.java
+++ src/main/java/com/spectra/jewel/JewelApplication.java
@@ -3,15 +3,22 @@ package com.spectra.jewel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ImportResource("classpath:integration-config.xml")
-public class JewelApplication {
+public class JewelApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JewelApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JewelApplication.class);
 	}
 
 }
